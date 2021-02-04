6c3ef6d8c23c20a2378738e1d7dd3d91f44ccc3e
Phil Bailey <philbailey@msn.com>
2021-02-03 21:47:33 -0500

    part 2 complete

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
 # .env
 import os
 from dotenv import load_dotenv
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, jsonify
 from models import db, connect_db, Cupcake
 
 # .env
@@ -16,3 +16,57 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql:///cupcake"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 connect_db(app)
+
+# ----------------------------------------------------------------------
+def serialize_cupcake(cupcake):
+    """takes db record and returns dict of db record"""
+    return {
+        "id": cupcake.id,
+        "flavor": cupcake.flavor,
+        "size": cupcake.size,
+        "rating": cupcake.rating,
+        "image": cupcake.image,
+    }
+
+
+# ----------------------------------------------------------------------
+@app.route("/api/cupcakes")
+def return_cupcakes():
+    """return list of cupcakes as JSON"""
+
+    # query all cupcake records from db
+    cupcakes = Cupcake.query.all()
+    # change db record objects into dict with serialize_cupcake function
+    serialized = [serialize_cupcake(cake) for cake in cupcakes]
+
+    return jsonify(cupcakes=serialized)
+
+
+# ----------------------------------------------------------------------
+@app.route("/api/cupcakes/<int:cupcake_id>")
+def return_cupcake(cupcake_id):
+    """return a cupcake as JSON"""
+
+    cupcake = Cupcake.query.get_or_404(cupcake_id)
+    serialized = serialize_cupcake(cupcake)
+
+    return jsonify(cupcake=serialized)
+
+
+# ----------------------------------------------------------------------
+@app.route("/api/cupcakes", methods=["POST"])
+def create_cupcake():
+    """makes a cupcake in db, return created record as json"""
+
+    flavor = request.json["flavor"]
+    rating = request.json["rating"]
+    size = request.json["size"]
+
+    cupcake = Cupcake(flavor=flavor, size=size, rating=rating)
+
+    db.session.add(cupcake)
+    db.session.commit()
+
+    serialized = serialize_cupcake(cupcake)
+
+    return (jsonify(cupcake=serialized), 201)
--- models.py
+++ models.py
@@ -20,3 +20,7 @@ class Cupcake(db.Model):
     size = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Float, nullable=False)
     image = db.Column(db.Text, nullable=False, default="https://tinyurl.com/demo-cupcake")
+
+    def __repr__(self):
+        c = self
+        return f"<Cupcake {c.id} {c.flavor} {c.size} {c.rating} {c.image}>"
